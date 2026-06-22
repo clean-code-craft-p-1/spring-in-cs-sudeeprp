5edb7b65f7c06ea62e43d585889798ad89ace60c
Sudeep Prasad <sudeep.rp@gmail.com>
2026-06-22 10:47:18 +0530

    first implementation

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -11,7 +12,7 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{98.6, 98.2, 97.8, 102.2});
+                new List<float>{98.6f, 98.2f, 97.8f, 102.2f});
             float epsilon = 0.001F;
             Assert.True(Math.Abs(computedStats.average - 99.2) <= epsilon);
             Assert.True(Math.Abs(computedStats.max - 102.2) <= epsilon);
@@ -22,11 +23,13 @@ namespace Statistics.Test
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
-                new List<___>{});
+                new List<float>{});
             // All fields of computedStats (average, max, min) must be
             // Double.NaN (not-a-number), as described in
             // https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
-            // Specify the Assert statements here
+            Assert.True(Double.IsNaN(computedStats.average));
+            Assert.True(Double.IsNaN(computedStats.max));
+            Assert.True(Double.IsNaN(computedStats.min));
         }
     }
 }
--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    public class Stats
+    {
+        public double average { get; set; }
+        public double max { get; set; }
+        public double min { get; set; }
+    }
+
     public class StatsComputer
     {
         public Stats CalculateStatistics(List<float> numbers)
         {
-            //Implement statistics here
+            if (numbers.Count == 0)
+            {
+                return new Stats
+                {
+                    average = double.NaN,
+                    max = double.NaN,
+                    min = double.NaN
+                };
+            }
+
+            double sum = 0;
+            double max = numbers[0];
+            double min = numbers[0];
+
+            foreach (float num in numbers)
+            {
+                sum += num;
+                if (num > max) max = num;
+                if (num < min) min = num;
+            }
+
+            double average = sum / numbers.Count;
+
+            return new Stats
+            {
+                average = average,
+                max = max,
+                min = min
+            };
         }
     }
 }
